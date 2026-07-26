6b2e08de910494d683cc34e60d39b1b3e87bdab7
juyoung710 <wndudquf@gmail.com>
2026-07-26 17:04:08 +0900

    유저 정보받아서 버튼 다르게 설정

--- Client/src/components/PostContent.jsx
+++ Client/src/components/PostContent.jsx
@@ -10,12 +10,16 @@ export default function PostContent({post_id}) {
   const [ post, setPost ] = useState("")
   // 게시글을 불러오는지 확인
   const [ loading, setLoading ] = useState(true)
+  // 유저
+  const [ user, setUser ] = useState(null)
   // 이미 가져온 게시글의 Id저장, 이미 요청한 게시글 ID를 기억해서 중복요청 방지
   const fetchedPostId = useRef(null)
 
   const API_URL = `http://127.0.0.1:18765/post/${post_id}`
   const token = localStorage.getItem("token")
+  const AUTH_API_URL = "http://127.0.0.1:18765/auth/me"
 
+  // 게시글 가져오기
   const fetchPosts = async () => {
     try {
       setError("")
@@ -51,7 +55,29 @@ export default function PostContent({post_id}) {
 
     fetchedPostId.current = post_id
     fetchPosts()
+    fetchUsers()
   }, [post_id])
+
+  // 유저 정보 가져오기
+  const fetchUsers = async () => {
+    try{
+      const response = await fetch(AUTH_API_URL, {
+        method: "GET",
+        headers: {
+          'Authorization' : `Bearer ${token}`
+        }
+      })
+      if(!response.ok){
+        throw new Error("유저 정보를 가져오지 못했습니다.")
+      }
+      const userData = await response.json()
+      setUser(userData)
+    }catch(error){
+      console.error("사용자 정보 조회 오류: ", error)
+      setError(error.message)
+    }
+
+  }
 
   // 게시글 삭제
   const handleDelete = async () => {
@@ -125,56 +151,79 @@ export default function PostContent({post_id}) {
       </article>
 
       <footer className={styles.actions}>
-        <button
-          className={styles.actionButton}
-          onClick={handleDelete}
-          type="submit"
-          aria-label="게시글 삭제"
-          title="삭제"
-        >
-          <svg
-            className={styles.actionIcon}
-            viewBox="0 0 32 32"
-            aria-hidden="true"
+        {user?.userid === post?.userid ? (
+          <>
+          <button
+            className={styles.actionButton}
+            onClick={handleDelete}
+            type="button"
+            aria-label="게시글 삭제"
+            title="삭제"
           >
-            <path d="M7 9h18" />
-            <path d="M13 5h6l1 4h-8l1-4Z" />
-            <path d="m9 9 1.5 18h11L23 9" />
-            <path d="M14 14v8m4-8v8" />
-          </svg>
-        </button>
-        <button
-          className={styles.actionButton}
-          type="button"
-          aria-label="게시글 수정"
-          title="수정"
-        >
-          <svg
-            className={styles.actionIcon}
-            viewBox="0 0 32 32"
-            aria-hidden="true"
+            <svg
+              className={styles.actionIcon}
+              viewBox="0 0 32 32"
+              aria-hidden="true"
+            >
+              <path d="M7 9h18" />
+              <path d="M13 5h6l1 4h-8l1-4Z" />
+              <path d="m9 9 1.5 18h11L23 9" />
+              <path d="M14 14v8m4-8v8" />
+            </svg>
+          </button>
+          <button
+            className={styles.actionButton}
+            type="button"
+            aria-label="게시글 수정"
+            title="수정"
           >
-            <path d="m7 23-1 4 4-1L25 11l-3-3L7 23Z" />
-            <path d="m19.5 10.5 3 3" />
-          </svg>
-        </button>
-        <button
-          className={styles.actionButton}
-          type="button"
-          aria-label="게시글 공유"
-          title="공유"
-          onClick={handleShare}
-        >
-          <svg
-            className={styles.actionIcon}
-            viewBox="0 0 32 32"
-            aria-hidden="true"
+            
+            <svg
+              className={styles.actionIcon}
+              viewBox="0 0 32 32"
+              aria-hidden="true"
+            >
+              <path d="m7 23-1 4 4-1L25 11l-3-3L7 23Z" />
+              <path d="m19.5 10.5 3 3" />
+            </svg>
+          </button>
+          <button
+            className={styles.actionButton}
+            type="button"
+            aria-label="게시글 공유"
+            title="공유"
+            onClick={handleShare}
           >
-            <rect x="10" y="5" width="15" height="19" rx="2" />
-            <path d="M10 10H7a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2v-1" />
-            <path d="M17.5 17.5V10m0 0-3 3m3-3 3 3" />
-          </svg>
-        </button>
+            <svg
+              className={styles.actionIcon}
+              viewBox="0 0 32 32"
+              aria-hidden="true"
+            >
+              <rect x="10" y="5" width="15" height="19" rx="2" />
+              <path d="M10 10H7a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2v-1" />
+              <path d="M17.5 17.5V10m0 0-3 3m3-3 3 3" />
+            </svg>
+          </button> 
+          </>
+          ) : 
+          <button
+            className={styles.actionButton}
+            type="button"
+            aria-label="게시글 공유"
+            title="공유"
+            onClick={handleShare}
+          >
+            <svg
+              className={styles.actionIcon}
+              viewBox="0 0 32 32"
+              aria-hidden="true"
+            >
+              <rect x="10" y="5" width="15" height="19" rx="2" />
+              <path d="M10 10H7a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2v-1" />
+              <path d="M17.5 17.5V10m0 0-3 3m3-3 3 3" />
+            </svg>
+          </button> 
+          } 
       </footer>
     </section>
   )
