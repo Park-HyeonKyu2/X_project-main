2e29c5204f682d0c0c691fde8a0eb009712f6238
이희승 <heseung0895@naver.com>
2026-07-26 18:37:00 +0900

    모달 완성

---
+++ Client/src/components/LoginModal.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import styles from "./LoginModal.module.css"
+import { Link } from "react-router-dom"
+
+const API_URL = "http://127.0.0.1:18765/auth/login"
+
+// 로그인이랑 똑같음 단 모달창 닫게 onClose 받음
+
+export default function LoginModal({ onClose }) {
+    const [error, setError] = useState("")
+    const [userid, setUserid] = useState("")
+    const [password, setPassword] = useState("")
+
+    const fetchLogin = async () => {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userid, password }),
+        })
+
+        const data = await response.json()
+
+        if (!response.ok) {
+            throw new Error("계정 정보를 불러오지 못했습니다.")
+        }
+        return data
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError("")
+
+        try {
+            const data = await fetchLogin()
+            localStorage.setItem("token", data.token)
+
+            onClose()   // 모달창만 닫음           
+        } catch (error) {
+            console.error(error)
+            setError(error.message)
+        }
+    }
+
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <form onSubmit={handleSubmit}>
+                    <p>로그인</p>
+                    <input className={styles.input} type="text" placeholder="아이디를 입력해주세요" value={userid} onChange={(e) => setUserid(e.target.value)} />
+
+                    <p>비밀번호</p>
+                    <input className={styles.input} type="password" placeholder="비밀번호를 입력해주세요" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                    {error && <p>{error}</p>}
+
+                    <button className={styles.button}>로그인</button>
+
+                    <p>아이디가 아직 없으신가요?</p>
+                    <Link to="/auth/signup">회원가입</Link>
+                </form>
+            </div>
+        </div>
+    )
+}
--- Client/src/pages/post.jsx
+++ Client/src/pages/post.jsx
@@ -3,15 +3,37 @@ import styles from "./post.module.css"
 import PostContent from "../components/PostContent"
 import ComentSection from "../components/ComentSection"
 
+// 모달을 위한 import
+import { useState, useEffect } from "react"
+import LoginModal from "../components/LoginModal.jsx"
+
 export default function Post() {
   const { post_id } = useParams()
 
+  // 모달
+  const [isLoginModalOpen, setLoginModal] = useState(false) // 모달이 열려 있냐/모달 열까 말까
+  const token = localStorage.getItem("token") // 토큰 가져와서 로그인 여부 판단
+
+  // 한 번만 띄울거임
+  useEffect(() => {
+    if (!token) {
+      setLoginModal(true) // 여기서 isLoginModalOpen도 같이 true로 바뀜
+    }
+  }, [])
+
+  const isCloseModal = () => {
+    setLoginModal(false)
+  }
+
+  // isLoginModalOpen 이게 true면 모달창 띄움 그리고 onClose 넘겨줌
   return (
     <>
+      {isLoginModalOpen && <LoginModal onClose={isCloseModal} />}
+
       <h4>post_id: {post_id}</h4>
       <section className={styles.section}>
-          <PostContent post_id={post_id} />
-          <ComentSection post_id={post_id} />
+        <PostContent post_id={post_id} />
+        <ComentSection post_id={post_id} />
       </section>
     </>
   )
